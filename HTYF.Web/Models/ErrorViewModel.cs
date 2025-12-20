63db88fd9487fb147285028df14b1f954187d368
Kayode Abereowo <kayodeabereowo@gmail.com>
2025-12-20 15:20:27 +0100

    Add project files.

---
+++ HTYF.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTYF.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
